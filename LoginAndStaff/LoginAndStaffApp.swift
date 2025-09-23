661780a7e4ecb8c919b50a8fb4491b0ce13695da
Jeffrey Cheung <jeffreycheung1995@gmail.com>
2025-09-23 19:48:42 +0800

    Initial Commit

---
+++ LoginAndStaff/LoginAndStaffApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAndStaffApp.swift
+//  LoginAndStaff
+//
+//  Created by Jeffrey Cheung on 23/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoginAndStaffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginAndStaffTests/LoginAndStaffTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAndStaffTests.swift
+//  LoginAndStaffTests
+//
+//  Created by Jeffrey Cheung on 23/9/2025.
+//
+
+import Testing
+@testable import LoginAndStaff
+
+struct LoginAndStaffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
